4fcdc712844a6c97b961c32cc90c1eeb7832c752
NethminiDevyanjalee <nethminid1999@gmail.com>
2021-08-06 16:43:08 +0530

    play with outputs

--- kmain.c
+++ kmain.c
@@ -1,4 +1,16 @@
+#include "framebuffer.h"
+#include "serial.h"
+
 int kmain()
-    {
-        return 0;
-    }
+{
+	char msg[] = "Welcome to myFirstOS";
+	unsigned int len = sizeof(msg) / sizeof(msg[0]);
+	
+	//frame builder writing
+    	fb_write(0, msg, len);
+    
+    	//serial writing
+    	serial_write(0x3F8, msg, len);
+    	
+	return 0;
+} 
